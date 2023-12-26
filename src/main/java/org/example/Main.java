ea50f4bda2c6421abf43400f8879b2544b30a051
Lysepten <tlstlsrbrb11@gamil.com>
2023-12-26 15:46:00 +0900

    테스트케이스와 assertThat 라이브러리 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,9 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
   public static void main(String[] args) {
-
-    System.out.printf("Hello and welcome!\n");
-
-    Scanner sc = new Scanner(System.in);
-
-    for (int i = 1; i <= 2; i++) {
-      for (int k = 1; k <= 4; k++) {
-        System.out.print("*");
-      }
-      System.out.println();
-    }
-
+    System.out.println(
+    new App().plus(10, 20)
+    );
   }
 }
